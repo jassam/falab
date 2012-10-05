824ff6d5a25b1be7b2b2cafdc4c34e45f498fd6d
luolongzhi <luolongzhi@6892ef35-3739-5941-6b8f-8589d261233f>
2012-10-05 09:58:10 +0000

    merge trunk

--- src/project/fa_aacenc/fa_parseopt.c
+++ src/project/fa_aacenc/fa_parseopt.c
@@ -40,8 +40,8 @@
 
 
 /*global option vaule*/
-/*char  opt_inputfile[256]  = "xs-mono.wav";*/
-char  opt_inputfile[256]  = "mfs.wav";
+char  opt_inputfile[256]  = "xs-mono.wav";
+/*char  opt_inputfile[256]  = "mfs.wav";*/
 char  opt_outputfile[256] = "xs-out.wav";
 int   opt_framelen  = 1024;
 
