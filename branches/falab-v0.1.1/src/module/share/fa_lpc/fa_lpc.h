51eecbede48c66261685f15a77219431809742e3
luolongzhi <luolongzhi@6892ef35-3739-5941-6b8f-8589d261233f>
2012-11-17 16:08:02 +0000

    add fa_lpc

---
+++ branches/falab-v0.1.1/src/module/share/fa_lpc/fa_lpc.h
@@ -0,0 +1,55 @@
+/*
+  falab - free algorithm lab 
+  Copyright (C) 2012 luolongzhi 罗龙智 (Chengdu, China)
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+  filename: fa_lpc.h 
+  version : v1.0.0
+  time    : 2012/11/17 23:52
+  author  : luolongzhi ( falab2012@gmail.com luolongzhi@gmail.com )
+  code URL: http://code.google.com/p/falab/
+
+*/
+
+#ifndef _FA_LPC_H
+#define _FA_LPC_H 
+
+#ifdef __cplusplus 
+extern "C"
+{ 
+#endif  
+
+//#define USE_LPC_HP
+
+typedef unsigned uintptr_t;
+
+uintptr_t fa_lpc_init(int p);
+void      fa_lpc_uninit(uintptr_t handle);
+
+#ifdef USE_LPC_HP 
+int fa_lpc(uintptr_t handle, double *x, int x_len, double *lpc_cof, double *err);
+#else 
+int fa_lpc(uintptr_t handle, float *x, int x_len, float *lpc_cof, float *err);
+#endif
+ 
+
+#ifdef __cplusplus 
+}
+#endif  
+
+
+
+#endif
