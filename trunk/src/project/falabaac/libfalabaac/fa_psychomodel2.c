7d12db641d244f5c4ed89f62ba79e1c48fc3c1de
luolongzhi <luolongzhi@6892ef35-3739-5941-6b8f-8589d261233f>
2013-02-24 09:02:56 +0000

    ref para1

--- trunk/src/project/falabaac/libfalabaac/fa_psychomodel2.c
+++ trunk/src/project/falabaac/libfalabaac/fa_psychomodel2.c
@@ -873,7 +873,7 @@ void fa_psychomodel2_calculate_pe_improve(uintptr_t handle, float *x, float *pe,
             /*nb[i] = FA_MAX((pow(10., qsthr[i]/10.)), 1.6*nb_prev[i]);*/
 
             /*nb[i] = FA_MAX((pow(10., qsthr[i]/10.)), FA_MIN(nb[i], 1.*nb_prev[i]));*/
-            nb[i] = FA_MAX((pow(10., qsthr[i]/10.)), FA_MIN(nb[i], 1.2*nb_prev[i]));
+            nb[i] = FA_MAX((pow(10., qsthr[i]/10.)), FA_MIN(nb[i], 1.*nb_prev[i]));
             /*nb[i] = 1.6*nb_prev[i]; //+0.02*nb[i];//pow(10., qsthr[i]/10.);*/
 
 #endif
